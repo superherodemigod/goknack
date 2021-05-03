ba98cf1fcaa3e23fffcc67840a3af24e0928108a
Napoleon <sherlockdev0105@gmail.com>
2021-05-03 03:39:34 +0200

    delete files

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
-import menuImg from './imgs/menu-btn.png';
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <button className="menuBtn"><img className="menuImg" src={menuImg} alt=""/></button>
-                <div className="balance">
-                    <span>$9,999,999</span>
-                </div>
-                <div className="bet">
-                    <span>$100,000,000</span>
-                </div>
+                
             </div>
         )
     }
