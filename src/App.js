78db84ba968d60caf50d68b49177f4fbb6f9ab10
Napoleon <sherlockdev0105@gmail.com>
2021-05-03 06:03:45 +0200

    add images and make the style of header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import "./App.css";
+import "./App.scss";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <Header />
-        </div>
+        <Header />
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import MenuBtn from './image/menu-btn.png'
+import Logo from './image/logo.png'
 class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="header">
+        <div className="header-left">
+          <img className="menu-btn" src={MenuBtn} alt="" width="74" height="48" />
+          <img className="logo" src={Logo} alt="" width="139" height="28" />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
