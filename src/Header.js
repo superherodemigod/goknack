5ba6253bd22abe0cba38d7172de3f9b1ce675b4d
Napoleon <sherlockdev0105@gmail.com>
2021-05-03 11:56:13 +0200

    add search bar

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
-import MenuBtn from './image/menu-btn.png'
-import Logo from './image/logo.png'
+import MenuBtn from "./image/menu-btn.png";
+import Logo from "./image/logo.png";
 class Header extends Component {
   render() {
     return (
       <div className="header">
         <div className="header-left">
-          <img className="menu-btn" src={MenuBtn} alt="" width="74" height="48" />
-          <img className="logo" src={Logo} alt="" width="139" height="28" />
+          <div className="menu-btn">
+            <img src={MenuBtn} alt="" />
+          </div>
+          <div className="logo">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="search_bar">
+              
+          </div>
         </div>
       </div>
     );
