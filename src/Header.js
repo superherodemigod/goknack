02314865ab2cf62e50dec66334f6b0d37e69f7ae
Napoleon <sherlockdev0105@gmail.com>
2021-05-03 18:29:31 +0200

    update header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import MenuBtn from "./image/menu-btn.png";
 import Logo from "./image/logo.png";
+import KMark from "./image/k-mark.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 class Header extends Component {
   render() {
     return (
@@ -18,6 +21,14 @@ class Header extends Component {
                 Dog
               </div>
               <div className="search_badge">
+                <div className="k-mark">
+                  <img src={KMark} alt=""/>
+                  <div className="k-text">0.15</div>
+                </div>
+                <div className="rise-mark">
+                  <FontAwesomeIcon className="rise-icon" icon={faArrowUp} />
+                  <div className="rise-text">13%</div>
+                </div>
               </div>
             </div>
           </div>
