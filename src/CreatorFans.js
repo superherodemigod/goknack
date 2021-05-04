a96125ee84fbb44a0116d01711075b9b91026f09
Napoleon <sherlockdev0105@gmail.com>
2021-05-04 05:40:09 +0200

    update style of header text

--- src/CreatorFans.js
+++ src/CreatorFans.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CreatorImg from "./image/creator-icon.png";
 import FanImg from "./image/fan-icon.png";
+import BookImg from "./image/content-icon.png";
 class CreatorFans extends Component {
   render() {
     return (
@@ -13,8 +14,8 @@ class CreatorFans extends Component {
                 <img src={CreatorImg} alt="" />
               </div>
               <div className="text_creator">
-                <div className="text_header">how to</div>
-                <div className="text_content">become a creator</div>
+                <div className="text_title">how to</div>
+                <div className="text_content">Become a Creator</div>
               </div>
             </div>
             <div className="item_fan">
@@ -22,8 +23,25 @@ class CreatorFans extends Component {
                 <img src={FanImg} alt="" />
               </div>
               <div className="text_fan">
-                <div className="text_header">how to</div>
-                <div className="text_content">become a creator</div>
+                <div className="text_title">how to</div>
+                <div className="text_content">Become a Creator</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="creator_fans_content">
+          <div className="container">
+            <div className="img_book">
+              <img src={BookImg} alt="" />
+            </div>
+            <div className="text-header">
+              <div className="text-title">Becoming a Creator on GoKnack</div>
+              <div className="text-content">
+                As a creator, wouldn’t it be great to share your work across
+                different platforms and earn passive income as a fan admires and
+                engages with your work?! Well now you can, let’s compare! Use
+                our share widget and receive a minimum $.06 per engagement.
+                Share on Facebook, Twitter and any other social media platform!
               </div>
             </div>
           </div>
